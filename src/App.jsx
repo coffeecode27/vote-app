aff0f179f6b008918d0517dc820e09cd60d2a69e
coffeecode <imamsuranda@gmail.com>
2024-02-14 18:40:58 +0700

    menambahkan routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import {
+  HashRouter as Router, // Ganti BrowserRouter menjadi HashRouter
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import DisplayChart from "./pages/displayChartPage/index.jsx";
+
 function App() {
-  return <DisplayChart />;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/display/vote/pemilu" />} />
+          <Route path="/display/vote/pemilu" element={<DisplayChart />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
